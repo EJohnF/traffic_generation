70b8ebc90c546037617b640e725c8d8da06bb560
Eugene Eustropov <john0606@yandex.ru>
2017-02-08 14:57:05 +0300

    code for packet creating

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pcap.h>
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h> /* includes net/ethernet.h */
+
 using namespace std;
 
 static void load_finished_cb(WebKitWebView *web_view, gpointer data)
@@ -65,7 +72,6 @@ void work_with_packets()
     char errbuf[PCAP_ERRBUF_SIZE];
     u_char packet[100];
     int i;
-    std::cout<<"try to open device";
         /* Open the output device */
         if ( (fp= pcap_open_live("wlan0",            // name of the device
                             100,                // portion of the packet to capture (only the first 100 bytes)
@@ -113,10 +119,77 @@ void work_with_packets()
         return;
 }
 
+int sniff_packet()
+{
+    pcap_t *handle;			/* Session handle */
+    char *dev = "wlan0";			/* The device to sniff on */
+    char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+    struct pcap_pkthdr header;	/* The header that pcap gives us */
+    const u_char *packet;		/* The actual packet */
+struct ether_header *eptr;  /* net/ethernet.h */
+    int i;
+        u_char *ptr; /* printing out hardware header info */
+
+        /* Open the session in promiscuous mode */
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return(2);
+    }
+
+    /* Grab a packet */
+    packet = pcap_next(handle, &header);
+    /* Print its length */
+    printf("Jacked a packet with length of [%d]\n", header.len);
+    printf("Recieved at ..... %s\n",ctime((const time_t*)&header.ts.tv_sec));
+    printf("Ethernet address length is %d\n",ETHER_HDR_LEN);
+    /* And close the session */
+
+    eptr = (struct ether_header *) packet;
+
+      /* Do a couple of checks to see what packet type we have..*/
+      if (ntohs (eptr->ether_type) == ETHERTYPE_IP)
+      {
+          printf("Ethernet type hex:%x dec:%d is an IP packet\n",
+                  ntohs(eptr->ether_type),
+                  ntohs(eptr->ether_type));
+      }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP)
+      {
+          printf("Ethernet type hex:%x dec:%d is an ARP packet\n",
+                  ntohs(eptr->ether_type),
+                  ntohs(eptr->ether_type));
+      }else {
+          printf("Ethernet type %x not IP", ntohs(eptr->ether_type));
+          exit(1);
+      }
+
+      /* copied from Steven's UNP */
+      ptr = eptr->ether_dhost;
+      i = ETHER_ADDR_LEN;
+      printf(" Destination Address:  ");
+      do{
+          printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
+      }while(--i>0);
+      printf("\n");
+
+      ptr = eptr->ether_shost;
+      i = ETHER_ADDR_LEN;
+      printf(" Source Address:  ");
+      do{
+          printf("%s%x",(i == ETHER_ADDR_LEN) ? " " : ":",*ptr++);
+      }while(--i>0);
+      printf("\n");
+
+
+    pcap_close(handle);
+    return(0);
+}
+
 int main( int argc, char **argv)
 {
 //    generate_queries (argc, argv);
-    work_with_packets();
+//    work_with_packets();
+    sniff_packet();
 }
 
 
