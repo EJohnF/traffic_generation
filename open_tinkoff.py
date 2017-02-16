09e1e57a2cf28c4230882297a4e75a072d4b18cb
Eugene Eustropov <john0606@yandex.ru>
2017-02-16 16:10:29 +0300

    add python script for open web page. Add shell script for creating new network namespace and new virtual network interfaces and set it all up

--- open_tinkoff.py
+++ open_tinkoff.py
@@ -5,5 +5,9 @@ view = webkit.WebView()
 sw = gtk.ScrolledWindow() 
 sw.add(view) 
 
+win = gtk.Window(gtk.WINDOW_TOPLEVEL) 
+win.add(sw) 
+win.show_all() 
+
 view.open("http://tinkoff.ru/") 
 gtk.main()
