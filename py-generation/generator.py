ee75d17ea9ddae8995f58350dd92ed82f4998aca
Eugene Eustropov <john0606@yandex.ru>
2017-02-22 13:56:45 +0300

    working script for traffic generation

--- py-generation/generator.py
+++ py-generation/generator.py
@@ -1,5 +1,6 @@
 import json
 import random
+import numpy
 import gtk
 import webkit
 import google
@@ -18,12 +19,16 @@ sw.add(view)
 def thread_open():
     gtk.main()
 
+threadList = []
+
 
 def open_page(link):
     print 'open_page', link
-    view.open("http://www."+link)
+    # view.open("http://www."+link)
+    view.open(link)
     thread = Thread(target=thread_open, args=( ))
     thread.start()
+    threadList.append(thread)
 
 random.seed(10)
 
@@ -32,29 +37,35 @@ def distribution_to_value(description):
     dis_type = description["type"]
     if dis_type == "uniform":
         return random.randint(int(description["low_boundary"]), int(description["up_boundary"]))
+    if dis_type == "log-normal":
+        return numpy.exp(numpy.random.normal(description["M"], description["D"]))
     return 0
 
 
 def generate_page_list(site_link, number_pages=10):
     result = []
     i = 0
-    # for url in google.search("site:" + site_link):
-    #     if i < number_pages:
-    #         print 'generated page: ', url
-    #         result.append(url)
-    #         i += 1
-    result.append(site_link)
-    result.append(site_link)
-    result.append(site_link)
+    for url in google.search("site:" + site_link, num=number_pages):
+        if i < number_pages:
+            print 'generated page: ', url
+            result.append(url)
+            i += 1
+        else:
+            break
+    print 'list generated'
     return result
 
 
 def generator(sites):
     for site in sites:
-        pages_list = generate_page_list(site)
+        number_page = distribution_to_value(config['schemes']['default']['page_number'])
+        print 'page_number ', number_page
+        pages_list = generate_page_list(site, int(number_page))
         for page in pages_list:
             open_page(page)
-            time.sleep(distribution_to_value(config['schemes']['default']['time_between_page']))
+            sleep_time = distribution_to_value(config['schemes']['default']['time_between_page'])
+            print 'sleep time: ', sleep_time
+            time.sleep(sleep_time)
 
 f = open('configuration.json', 'r')
 config = json.load(f)
